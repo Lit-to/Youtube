b0674b292f817511b7fa0ea5aea7b482b010443d
Lit-to <swordworld0327@gmail.com>
2023-07-21 21:39:28 +0900

    移行

--- extention/popup__.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("collectButton").addEventListener("click", function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.scripting.executeScript({
-            target: { tabId: tabs[0].id },
-            files: ["content.js"],
-        });
-    });
-});
