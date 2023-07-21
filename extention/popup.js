2d20c47fefd9fee3e02744f434f796aa89d47db3
Lit-to <litto0315@gmail.com>
2023-07-21 21:26:53 +0900

    エラー解決策

--- extention/popup.js
+++ extention/popup.js
@@ -1,8 +1,13 @@
-document.getElementById("collectButton").addEventListener("click", function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.scripting.executeScript({
-            target: { tabId: tabs[0].id },
-            files: ["content.js"],
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOMContentLoaded event triggered");
+    document.getElementById("collectButton").addEventListener("click", function () {
+        console.log("Button clicked");
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            console.log("Tab ID:", tabs[0].id);
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                files: ["content.js"],
+            });
         });
     });
 });
