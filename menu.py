4303fc406c12c071ac8fa9d4a85254d9d92f3c04
Lit-to <swordworld0327@gmail.com>
2023-09-08 06:33:18 +0900

    README更新

--- menu.py
+++ menu.py
@@ -5,12 +5,12 @@ dirname=os.path.join(path,"bugs")
 for i in os.listdir(dirname):
     if os.path.isdir(os.path.join(dirname,i)):
         dirlist.append(i)
-print(dirlist)
 
 numlist=list()
 for i in dirlist:
     if i.isdigit():
         numlist.append(i)
+
 
 titles=list()
 time_stamps=list()
@@ -22,18 +22,16 @@ for i in numlist:
         time_stamps.append(contents[1][4:-1])
         youtube_link.append(contents[2][4:-1])
 
-
-
 note=list()
 
 note.append("# 目次")
 note.append("\n")
-note.append("|項目|日付|タイトル|バグレポート|詳細ファイル(動画用台本)|動画リンク|")
+note.append("|項目(クリックで詳細ファイル)|日付|報告タイトル|バグレポートリンク|動画リンク|")
 note.append("\n")
 note.append("|--|--|--|--|--|--|")
 note.append("\n")
 
-for i in range(len(numlist)-1):
+for i in range(len(numlist)):
     a_line="|[MC-"+numlist[i]+"](bugs/"+numlist[i]+"/main.md)"+"|"+time_stamps[i]+"|"+titles[i]+"|"+"https://bugs.mojang.com/browse/MC-"+numlist[i]+"|"+youtube_link[i]+"|"
     note.append(a_line)
     note.append("\n")
@@ -41,7 +39,6 @@ for i in range(len(numlist)-1):
 with open(os.path.join(path,"menu.md"),mode="r",encoding="utf_8") as f_read:
     note=note+["\n"]+f_read.readlines()
 
-print(note)
 
 with open ("README.md",encoding="utf_8",mode="w") as f_menu:
     f_menu.writelines(note)
